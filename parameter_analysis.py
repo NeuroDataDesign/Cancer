c931c07eb24fb145cf3ef12be58b223fdc4e3527
Savannah's Mac <sunyuxuan2012@163.com>
2024-11-19 14:21:41 -0500

    change on pictures

--- parameter_analysis.py
+++ parameter_analysis.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = "Cancer_Max_features.csv"  #
+file_path = "Cancer_HF.csv"  #
 data = pd.read_csv(file_path)
 
-grouped = data.groupby("features_number").agg(['mean', 'std'])
+grouped = data.groupby("Honest_Fraction").agg(['mean', 'std'])
 
 methods = ["SVM", "MIGHT", "SPO-MIGHT"]
 colors = ["blue", "red", "green"]
@@ -20,12 +20,12 @@ for method, color, marker in zip(methods, colors, markers):
                  marker=marker, capsize=5, linestyle='-', linewidth=1)
 
 
-plt.title("Performance of SVM, MIGHT, SPO-MIGHT", fontsize=14)
-plt.xlabel("MAX Features", fontsize=12)
-plt.ylabel("s@98", fontsize=12)
-plt.xticks(grouped.index, fontsize=10)
-plt.yticks(fontsize=10)
-plt.legend(fontsize=10)
+# plt.title("Performance of SVM, MIGHT, SPO-MIGHT", fontsize=14)
+# plt.xlabel("MAX Features", fontsize=12)
+# plt.ylabel("s@98", fontsize=12)
+plt.xticks(grouped.index, fontsize=18)
+plt.yticks(fontsize=18)
+plt.legend(fontsize=18)
 plt.grid(True, linestyle='--', alpha=0.7)
 
 # 显示图表
